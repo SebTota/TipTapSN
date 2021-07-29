b7bf209919393362d369be43165aec98a711fd4b
SebTota <kidokyo1@gmail.com>
2021-07-29 19:02:10 -0400

    fixed incomplete GitHub Pages paths

--- dist/js/app.js
+++ dist/js/app.js
@@ -137,7 +137,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "TipTapSN/";
+/******/ 	__webpack_require__.p = "/TipTapSN/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
