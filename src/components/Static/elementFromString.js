df086700d439ec8cccc7b5a9778c9adaae160474
Seb <kidokyo1@gmail.com>
2024-10-19 13:10:31 -0700

    Removed v1 project files

--- src/components/Static/elementFromString.js
+++
@@ -1,7 +0,0 @@
-/*
- * https://github.com/ueberdosis/tiptap/blob/main/packages/core/src/utilities/elementFromString.ts
- */
-export function elementFromString(value) {
-  const wrappedValue = `<body>${value}</body>`;
-  return new window.DOMParser().parseFromString(wrappedValue, "text/html").body;
-}
